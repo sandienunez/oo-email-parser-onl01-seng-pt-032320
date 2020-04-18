fd4ef52d714c0229944401cbe9e564d15bca9d7c
Sandie Nunez <snunez@vt.edu>
2020-04-18 22:26:37 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -16,11 +16,3 @@ class EmailAddressParser
 end
 
 end
-
-emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-parser1 = EmailParser.new(emails1)
-parser1.parse 
-
-emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-parser2 = EmailParser.new(emails2)
-parser2.parse
